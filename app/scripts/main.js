ea6e12471760fd5b22dd153da0d637cf5d813b30
RSARVASIDDI\raju <rajusvsp@gmail.com>
2017-10-28 23:33:27 +0530

    bower.json and main.js

---
+++ app/scripts/main.js
@@ -0,0 +1,17 @@
+require.config({
+    paths: {
+        'jquery': 'vendor/jquery/dist/jquery',
+        'underscore': 'vendor/underscore/underscore',
+        'backbone': 'vendor/backbone/backbone'
+    },
+    shim: {
+        underscore: {
+            exports: '_'
+        },
+        backbone: {
+            exports: 'Backbone',
+            deps: ['jquery', 'underscore']
+        }
+    },
+    deps: ['jquery', 'underscore']
+});
